0fd8ad3375d92435ac5d4da0d31d0a403bc980d9
eora21 <eora21@naver.com>
2025-02-13 19:42:18 +0900

    refactor: 💡 웹소켓 어댑터가 익스프레스 세션 및 lazy delete 사용하도록 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,33 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { CustomWebSocketAdapter } from './common/ws-adapter/custom-web-socket-adapter';
+import * as session from 'express-session';
+import { DAY } from './common/definition/time';
+import { LazyDeleteRoomEventOperator } from './common/event/lazy-delete-room-event-operator';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.ORIGIN });
-  app.useWebSocketAdapter(new CustomWebSocketAdapter(app));
+
+  app.enableCors({ origin: process.env.ORIGIN, credentials: true });
+
+  const sessionMiddleware = session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: DAY,
+    }
+  });
+  app.use(sessionMiddleware);
+
+  const lazyDeleteRoomEventOperator = app.get(LazyDeleteRoomEventOperator);
+
+  app.useWebSocketAdapter(new CustomWebSocketAdapter(app, sessionMiddleware, lazyDeleteRoomEventOperator));
+
   await app.listen(process.env.PORT ?? 8080);
 }
 
